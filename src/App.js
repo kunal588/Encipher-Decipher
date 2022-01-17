6297e912db21d36de3fab0286def63834115361f
Your Name <you@example.com>
2022-01-17 13:41:56 +0530

    updated readme file

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/alt-text */
 import './App.css';
 import React from 'react';
 import table from './table'
@@ -10,25 +11,36 @@ class App extends React.Component{
     this.handleSubmit = this.handleSubmit.bind(this);
 
   }
-  algo(text){
-      var new_text='';
+  //algortihm to encrypt/decrypt the entered text
+  algorithm(text){
+      //initializing the empty string for storing the trasformation result
+      var result='';
+      //converting the input text to lowercase
       text=text.toLowerCase();
+      // iterating the chracters in the input text 
       for( let i in text){
+        //if its free space keeping  is as it is and appending it to result
         if(text[i]===' ') { 
-          new_text+=' '
+          result+=' '
           continue;
         }
-        console.log(text[i]);
-        new_text+=table.get(text[i]);
+        //if char and then appending the character corresponding to this char from lookup table
+        result+=table.get(text[i]);
       }
-      return new_text;
+      return result;
   }
+  //event handler function is fired when button(encrypt/decrypt) is pressed.
   handleSubmit(event){    
+    //finding the source of the event( plain_text or cipher_text)
      const name =(event.target.name ==='plain_text')?'cipher_text':'plain_text';
-     const value=(event.target.name ==='plain_text')? this.algo(this.state.plain_text):this.algo(this.state.cipher_text);
-    this.setState({...this.state,[name]:value});
+     //acc. to event the algorithm is used to encrypt/decrypt
+     const value=(event.target.name ==='plain_text')? this.algorithm(this.state.plain_text):this.algorithm(this.state.cipher_text);
+    //current state is updated 
+     this.setState({...this.state,[name]:value});
     event.preventDefault();
   }
+
+  //evnet handler to keep updating the state whenever input is changed
   handleChange(event){
     const {name , value} = event.target;
     console.log(event.target);
@@ -38,19 +50,25 @@ class App extends React.Component{
   render(){
     return (
     <div >
-    <h1 className='heading'>Encrypt/Decrypt it!!</h1>
-    <div className="App">
+        <h1 className='heading'>Encrypt/Decrypt it!!</h1>
 
-       <form name="plain_text" onSubmit={this.handleSubmit}>
-       <textarea class="longInput" placeholder='Enter the plain text here' name="plain_text" type="text" value={this.state.plain_text} onChange={this.handleChange} cols="30" rows="10"></textarea> 
-         <input className='button' type="submit" value="Encrypt"/>
-       </form>
-       <form name="cipher_text" onSubmit={this.handleSubmit}>
-       <textarea class="longInput" placeholder='Enter the cipher text here' name="cipher_text" type="text" value={this.state.cipher_text} onChange={this.handleChange} cols="30" rows="10"></textarea>
-         <input className='button' type="submit" value="Decrypt"/>
-       </form>
+        <div className="App">
 
-    </div>
+              <form name="plain_text" onSubmit={this.handleSubmit}>
+
+                <textarea class="longInput" placeholder='Enter the plain text here' name="plain_text" type="text" value={this.state.plain_text} onChange={this.handleChange} cols="30" rows="10"></textarea> 
+                <input className='button' type="submit" value="Encrypt"/>
+
+              </form>
+
+              <form name="cipher_text" onSubmit={this.handleSubmit}>
+
+                <textarea class="longInput" placeholder='Enter the cipher text here' name="cipher_text" type="text" value={this.state.cipher_text} onChange={this.handleChange} cols="30" rows="10"></textarea>
+                <input className='button' type="submit" value="Decrypt"/>
+
+              </form>
+
+        </div>
     </div>
   );
   }
